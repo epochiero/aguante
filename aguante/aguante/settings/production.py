75be6f31ffdb44aa8feff48380694add1fcb4f09
Ezequiel Pochiero <epochiero@gmail.com>
2014-11-28 11:57:50 -0300

    Add prod settings

---
+++ aguante/aguante/settings/production.py
@@ -0,0 +1,17 @@
+import os
+from .base import *
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.environ['DATABASE_HOST'],
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+    }
+}
