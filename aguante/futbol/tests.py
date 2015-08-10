8867e511d5b55a084d01d1bd97b02c6acb4d07f6
Ezequiel Pochiero <epochiero@gmail.com>
2015-08-10 16:42:04 -0300

    Add tests for futbol app

--- aguante/futbol/tests.py
+++ aguante/futbol/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from futbol.models import *
 
-# Create your tests here.
+
+class TestEquipos(TestCase):
+
+    def setUp(self):
+        # Crear torneos primero
+        self.torneo_20 = Torneo.objects.create(
+            nombre="Torneo Transicion 2014", universofutbol_id="896")
+        self.torneo_30 = Torneo.objects.create(
+            nombre="Torneo 2015", universofutbol_id="945")
+
+    def test_cargar_equipos(self):
+        self.torneo_20.cargar_equipos()
+        self.assertTrue(self.torneo_20.equipos_cargados)
+        self.assertEqual(self.torneo_20.equipos.count(), 20)
+
+        self.torneo_30.cargar_equipos()
+        self.assertTrue(self.torneo_30.equipos_cargados)
+        self.assertEqual(self.torneo_30.equipos.count(), 30)
+
+        # cargar_equipos() es idempotente
+        self.torneo_20.cargar_equipos()
+        self.assertTrue(self.torneo_20.equipos_cargados)
+        self.assertEqual(self.torneo_20.equipos.count(), 20)
+
+        self.torneo_30.cargar_equipos()
+        self.assertTrue(self.torneo_30.equipos_cargados)
+        self.assertEqual(self.torneo_30.equipos.count(), 30)
+
+    def test_cargar_fechas(self):
+        self.torneo_20.cargar_fechas()
+        self.assertEqual(self.torneo_20.fechas.count(), 19)
+
+        self.torneo_30.cargar_fechas()
+        self.assertEqual(self.torneo_30.fechas.count(), 30)
+
+        # cargar_fechas() es idempotente
+        self.torneo_20.cargar_fechas()
+        self.assertEqual(self.torneo_20.fechas.count(), 19)
+
+        self.torneo_30.cargar_fechas()
+        self.assertEqual(self.torneo_30.fechas.count(), 30)
