9b136d0d843937f3e6072e5d3d6a2895975a9e4b
Ezequiel Pochiero <epochiero@gmail.com>
2015-08-10 23:03:03 -0300

    Add admin actions for Torneo

--- aguante/futbol/admin.py
+++ aguante/futbol/admin.py
@@ -4,4 +4,21 @@ from futbol.models import Equipo, Fecha, Partido, Torneo
 admin.site.register(Equipo)
 admin.site.register(Fecha)
 admin.site.register(Partido)
-admin.site.register(Torneo)
+
+
+def admin_cargar_equipos(modeladmin, request, queryset):
+    for torneo in queryset:
+        torneo.cargar_equipos()
+admin_cargar_equipos.short_description = "Cargar equipos del torneo"
+
+
+def admin_cargar_fechas(modeladmin, request, queryset):
+    for torneo in queryset:
+        torneo.cargar_fechas()
+admin_cargar_fechas.short_description = "Cargar fechas del torneo"
+
+
+@admin.register(Torneo)
+class TorneoAdmin(admin.ModelAdmin):
+    exclude = ('equipos_cargados', 'cantidad_fechas',)
+    actions = (admin_cargar_equipos, admin_cargar_fechas,)
