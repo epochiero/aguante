f659fcd81a4d8bfc9e250858a9a82d1752e7f972
Ezequiel Pochiero <epochiero@gmail.com>
2015-08-16 14:25:20 -0300

    Fix match status

--- aguante/frontend/templatetags/frontend_tags.py
+++ aguante/frontend/templatetags/frontend_tags.py
@@ -7,10 +7,8 @@ register = template.Library()
 
 @register.filter()
 def icono_estado(partido):
-    """Devuelve un <div> con el icono de estado del partido"""
-    estado = "terminado"
     if partido.estado == EstadoPartido.NO_EMPEZADO:
-        estado = "noempezado"
+        return "noempezado"
     elif partido.estado == EstadoPartido.EN_JUEGO:
-        estado = "enjuego"
-    return mark_safe('<div class="icono-estado icono-estado-{}"></div>'.format(estado))
+        return "enjuego"
+    return "terminado"
