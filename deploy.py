58054588d24a9221672200f947e4d60663753990
Ezequiel Pochiero <epochiero@gmail.com>
2015-08-16 14:50:39 -0300

    Remove redundant directory

--- deploy.py
+++ deploy.py
@@ -26,7 +26,8 @@ class Task():
         user_config = ssh_config.lookup(self.env)
         return {'username': user_config['user'],
                 'hostname': user_config['hostname'],
-                'key_filename': user_config['identityfile'][0]
+                'key_filename': user_config['identityfile'][0],
+                'port': int(user_config['port'])
                 }
 
     def execute(self, cmd):
@@ -50,9 +51,9 @@ class Task():
         if exec_path:
             full_cmd += "cd %s; " % (exec_path)
         if virtualenv:
-            full_cmd += "source /home/django/.virtualenvs/%s/bin/activate; " % (
+            full_cmd += "source /home/aguante/.virtualenvs/%s/bin/activate; " % (
                 virtualenv)
-        return full_cmd + "/bin/bash -l -c '%s'" %(cmd)
+        return full_cmd + "/bin/bash -l -c '%s'" % (cmd)
 
     @contextmanager
     def cd(self, path):
@@ -69,18 +70,34 @@ class Task():
 
 def sudo(cmd, user=None):
     if user:
-        return "sudo -u %s %s" %(user, cmd)
-    return "sudo %s" %(cmd)
+        return "sudo -u %s %s" % (user, cmd)
+    return "sudo %s" % (cmd)
 
 
 def deploy(env):
     task = Task(env)
-    with task.cd('/home/django/aguante/aguante'):
+    with task.cd('/home/aguante/aguante/aguante'):
         task.execute('git pull')
         with task.activate_virtualenv('aguante'):
             task.execute('pip install --upgrade -r requirements.txt')
-            task.execute('python manage.py syncdb')
+            task.execute('python manage.py migrate')
             task.execute('python manage.py collectstatic --noinput')
-        task.execute(sudo('touch uwsgi.reload'))
 
+
+def get_sytem_packages():
+    package_list = ['git', 'python3-pip', 'rabbitmq-server', 'nginx',
+                    'supervisor', 'libxml2-dev', 'libxslt-dev', 'python-dev',
+                    'zlib1g-dev']
+    return ' '.join(package_list)
+
+
+def provision(env):
+    task = Task(env)
+    task.execute(sudo('aptitude update'))
+    task.execute(sudo(
+        'DEBIAN_FRONTEND=noninteractive aptitude install -y {}'.format(get_sytem_packages())))
+    task.execute(sudo('pip3 install virtualenvwrapper'))
+    task.execute('virtualenv ~/.virtualenvs/aguante/')
+
+#provision('aguante')
 deploy('aguante')
